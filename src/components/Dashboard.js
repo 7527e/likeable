66885c8b39737e7ce35f2db7a1cd7e2f41f5bf16
Aayush Srivastava <92394355+7527e@users.noreply.github.com>
2022-04-08 22:18:50 +0530

    Added functionality to fetch the current status of the user

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,9 @@
-import { signOutHandler } from "../Firebase";
 import { doc, setDoc, getDoc } from "firebase/firestore"; 
 import { getAuth, updateProfile } from "firebase/auth";
 import Logout from "./Logout";
 import {db} from "../Firebase"
+import StatusUpdater from "./StatusUpdater";
+// import Status from "./Status";
 
 const auth = getAuth();
 const user = auth.currentUser;
@@ -15,6 +16,7 @@ async function addUserToDB(){
         isUserAnon: user.isAnonymous,
         photo: user.photoURL,
     });
+    console.log("This User is Added to the DB");
 }
 
 const Dashboard = () => {
@@ -25,10 +27,13 @@ const Dashboard = () => {
             This is Dashboard
             <br />
             {user.displayName}
-        <Logout />         
+        <Logout />    
+        <StatusUpdater />   
         </div>
      );
 }
  
 export default Dashboard;
 
+//add current user to firebase 
+// do it only first to first timers
